18df338eac326e558a48413eb9013b7aae7d2016
MaksimRomanovvv <109852966+MaxRomanov5@users.noreply.github.com>
2025-09-13 11:57:55 +0300

    add images

---
+++ artistshub/src/assets/hero/index.js
@@ -0,0 +1,37 @@
+import desk1 from "./desk1.png";
+import desk2 from "./desk2.png";
+import desk3 from "./desk3.png";
+import desk4 from "./desk4.png";
+import desk5 from "./desk5.png";
+import desk6 from "./desk6.png";
+import tab1 from "./tab1.jpg";
+import tab2 from "./tab2.jpg";
+import tab3 from "./tab3.jpg";
+import tab4 from "./tab4.png";
+import tab5 from "./tab5.png";
+import mob1 from "./mob1.jpg";
+import mob2 from "./mob2.jpg";
+import mob3 from "./mob3.jpg";
+import mob4 from "./mob4.jpg";
+import mob5 from "./mob5.jpg";
+import mob6 from "./mob6.png";
+
+export default {
+  desk1,
+  desk2,
+  desk3,
+  desk4,
+  desk5,
+  desk6,
+  mob1,
+  mob2,
+  mob3,
+  mob4,
+  mob5,
+  mob6,
+  tab1,
+  tab2,
+  tab3,
+  tab4,
+  tab5,
+};
--- artistshub/src/components/Hero/Hero.jsx
+++ artistshub/src/components/Hero/Hero.jsx
@@ -1,20 +1,50 @@
-import React from 'react';
-import s from './Hero.module.css';
-import images from '../../assets';
-
+import React from "react";
+import s from "./Hero.module.css";
+import images from "../../assets";
+import heroImg from "../../assets/hero";
+import { useMediaQuery } from "react-responsive";
 const Hero = () => {
-    return (
-        <div className={s.section}>
-            <div className={s.leftSide}>
-                <h1 className={s.description}>Discover Your Next Favorite Artist Today</h1>
-                <p>Dive into a world of music discoveries with ArtistsHub – your personal guide to the boundless universe of sound. Explore, filter, and learn about your favorite artists all in one place.</p>
-                <p>Explore Artists</p>
-            </div>
-            <div className={s.rigthSide}>
-
-            </div>
-        </div>
-    );
-}
+  const isMobile = useMediaQuery({ query: "(max-width:768px)" });
+  const isDesktop = useMediaQuery({ query: "(min-width:1440px)" });
+  console.log(isDesktop);
+  return (
+    <div className={s.section}>
+      <div className={s.leftSide}>
+        <h1 className={s.description}>
+          Discover Your Next Favorite Artist Today
+        </h1>
+        <p>
+          Dive into a world of music discoveries with ArtistsHub – your personal
+          guide to the boundless universe of sound. Explore, filter, and learn
+          about your favorite artists all in one place.
+        </p>
+        <p>Explore Artists</p>
+      </div>
+      <div className={s.rightSide}>
+        <ul className="firstWrap">
+          <li>
+            <img
+              src={
+                isMobile
+                  ? heroImg.mob1
+                  : isDesktop
+                  ? heroImg.desk1
+                  : heroImg.tab1
+              }
+              alt="artist"
+            />
+          </li>
+          <li>
+            <img src={images.people_two} alt="artist" />
+          </li>
+          <li>
+            <img src={images.people_three} alt="artist" />
+          </li>
+        </ul>
+        <ul className="secondWrap"></ul>
+      </div>
+    </div>
+  );
+};
 
 export default Hero;
