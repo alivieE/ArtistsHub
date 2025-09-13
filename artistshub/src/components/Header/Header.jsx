4e1369ee5d9da8b8968853564a062bd3c41e0b5f
alivieE <153372860+alivieE@users.noreply.github.com>
2025-09-13 18:54:14 +0300

    added burger menu and fixed header

--- artistshub/src/components/Header/Header.jsx
+++ artistshub/src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import s from "./Header.module.css";
 import images from "../../assets";
 
 const Header = () => {
+  const [isOpenMenu, setisOpenMenu] = useState(false);
   return (
     <div className="container">
       <div className={s.header}>
@@ -10,11 +11,48 @@ const Header = () => {
           <img src={images.Subtract} alt="logo" />
           <p>ArtistsHub</p>
         </div>
-        <div className={s.nav}>
-          <a>Artists</a>
-          <a>About Us</a>
-          <a>Reviews</a>
-        </div>
+
+
+
+          <div className={s.menu}>
+             <input
+          onClick={() => {
+            setisOpenMenu(!isOpenMenu);
+          }}
+          type="checkbox"
+          id="burgerCheckbox"
+          className={s.burgerCheckbox}
+          checked={isOpenMenu}
+        />
+
+              <label for="burgerCheckbox" className={s.burger}></label>
+              {isOpenMenu && <div className={s.wrap}></div>}
+                <div className={`${s.overlay} ${isOpenMenu ? s.open : ""}`}>
+                  <nav class={s.nav}>
+                    <ul class={s.menuList}>
+                        <li
+                class={s.menuItem}
+                onClick={() => {
+                  setisOpenMenu(!isOpenMenu);
+                }}
+              ><a href="#" className={s.menu_item}>Artists</a></li>
+                        <li
+                class={s.menuItem}
+                onClick={() => {
+                  setisOpenMenu(!isOpenMenu);
+                }}
+              ><a href="#" className={s.menu_item}>About Us</a></li>
+                        <li
+                class={s.menuItem}
+                onClick={() => {
+                  setisOpenMenu(!isOpenMenu);
+                }}
+              ><a href="#" className={s.menu_item}>Reviews</a></li>
+                        
+                    </ul>
+                  </nav>
+                </div>
+          </div>
       </div>
     </div>
   );
