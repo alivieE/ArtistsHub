8100e97bb993e12e9cd65baa025b4844d5879585
alivieE <153372860+alivieE@users.noreply.github.com>
2025-09-20 12:22:31 +0300

    add artistsList

--- artistshub/src/components/ArtistList/ArtistList.jsx
+++ artistshub/src/components/ArtistList/ArtistList.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import s from "./ArtistList.module.css";
+
 const ArtistList = () => {
   return (
     <div className={s.section}>
       <div className="container">
         <p className={s.span}>Artists</p>
         <h2 className={s.sectionTitle}>Explore Your New Favorite Artists</h2>
+        <ul>
+          <li>
+            <img></img>
+            <ul className={s.genresList}>
+              <li className={s.genres}></li>
+            </ul>
+            <p className={s.artistsName}></p>
+            <p className={s.artistsDescription}></p>
+          </li>
+        </ul>
       </div>
     </div>
   );
