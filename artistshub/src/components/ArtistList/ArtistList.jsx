ec57f78cae1dd670d1ee72aa003ad59752730636
MaksimRomanovvv <109852966+MaxRomanov5@users.noreply.github.com>
2025-09-20 12:38:43 +0300

    add load artists

--- artistshub/src/components/ArtistList/ArtistList.jsx
+++ artistshub/src/components/ArtistList/ArtistList.jsx
@@ -1,21 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import s from "./ArtistList.module.css";
 
 const ArtistList = () => {
+  const [artists, setArtists] = useState([]);
+
+  useEffect(() => {
+    fetch("https://sound-wave.b.goit.study/api/artists?limit=10&page=1")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setArtists(data.artists);
+      });
+  }, []);
   return (
     <div className={s.section}>
       <div className="container">
         <p className={s.span}>Artists</p>
         <h2 className={s.sectionTitle}>Explore Your New Favorite Artists</h2>
         <ul>
-          <li>
-            <img></img>
-            <ul className={s.genresList}>
-              <li className={s.genres}></li>
-            </ul>
-            <p className={s.artistsName}></p>
-            <p className={s.artistsDescription}></p>
-          </li>
+          {artists.length > 0 &&
+            artists.map((artist) => {
+              return (
+                <li>
+                  <img src={artist.strArtistThumb}></img>
+                  <ul className={s.genresList}>
+                    {artist.genres.map((genre) => {
+                      return <li className={s.genre}>{genre}</li>;
+                    })}
+                  </ul>
+                  <p className={s.artistsName}>{artist.strArtist}</p>
+                  <p className={s.artistsDescription}>
+                    {artist.strBiographyEN}
+                  </p>
+                </li>
+              );
+            })}
         </ul>
       </div>
     </div>
