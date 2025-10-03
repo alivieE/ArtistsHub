ea1b60e95fb992682916ab0b99d958edc19180dc
alivieE <153372860+alivieE@users.noreply.github.com>
2025-10-03 21:37:57 +0300

    added modal

--- artistshub/src/components/ArtistList/ModalArtist/ModalArtist.jsx
+++ artistshub/src/components/ArtistList/ModalArtist/ModalArtist.jsx
@@ -1,15 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import s from "./ModalArtist.module.css";
 
 const ModalArtist = ({ id }) => {
-  console.log(id);
+  const [artist, setArtist] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://sound-wave.b.goit.study/api/artists/${id}`, {})
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setArtist(data);
+      });
+  }, [id]);
+
   return (
     <div className={s.overlay}>
-      {/* <p className={s.artistsName}>{artist.strArtist}</p>
-                          <p className={s.artistsDescription}>
-                            {artist.strBiographyEN}
-                          </p> */}
-      <p>{id}</p>
+      {artist && (
+        <div className={s.section}>
+          <p className={s.name}>{artist.strArtist}</p>
+          <img src={artist.strArtistThumb} className={s.strImg} />
+          <div>
+            <p className={s.intFormedYear}>{artist.intFormedYear}</p>
+          </div>
+          <div>
+                  <p className={s.strGender}>{artist.strGender}</p>
+          </div>
+          <div>
+            <p className={s.members}></p>
+          </div>
+          <div>
+            <p className={s.strLabel}>{artist.strLabel}</p>
+          </div>
+          <div>
+            <p className={s.biography}>{artist.strBiographyEN}</p>
+          </div>
+
+          {artist.genres.map((genre) => {
+            return <li className={s.genre}>{genre}</li>;
+          })}
+          <div></div>
+        </div>
+      )}
     </div>
   );
 };
