c2a84f012a96a798fd9655af7589514abeca7ea5
MaksimRomanovvv <109852966+MaxRomanov5@users.noreply.github.com>
2025-10-04 11:29:55 +0300

    fix modal

--- artistshub/src/components/ArtistList/ModalArtist/ModalArtist.jsx
+++ artistshub/src/components/ArtistList/ModalArtist/ModalArtist.jsx
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 import s from "./ModalArtist.module.css";
 
 const ModalArtist = ({ id }) => {
-  console.log(id);
+  const [artist, setArtist] = useState(null);
+  console.log(artist);
 
   useEffect(() => {
     fetch(`https://sound-wave.b.goit.study/api/artists/${id}`, {})
@@ -16,17 +17,17 @@ const ModalArtist = ({ id }) => {
 
   return (
     <div className={s.overlay}>
-      {id && (
+      {artist && (
         <div className={s.section}>
-          <p className={s.name}>{id.strArtist}</p>
-          <img src={id.strArtistThumb} className={s.strImg}></img>
+          <p className={s.name}>{artist.strArtist}</p>
+          <img src={artist.strArtistThumb} className={s.strImg}></img>
           <div>
             <p>Years active</p>
-            <p className={s.intFormedYear}>{id.intFormedYear}</p>
+            <p className={s.intFormedYear}>{artist.intFormedYear}</p>
           </div>
           <div>
             <p>Sex</p>
-            <p className={s.strGender}>{id.strGender}</p>
+            <p className={s.strGender}>{artist.strGender}</p>
           </div>
           <div>
             <p>Country</p>
@@ -34,13 +35,13 @@ const ModalArtist = ({ id }) => {
           </div>
           <div>
             <p>Biography</p>
-            <p className={s.strLabel}>{id.strLabel}</p>
+            <p className={s.strLabel}>{artist.strLabel}</p>
           </div>
           <div>
             <p>Years active</p>
-            <p className={s.biography}>{id.strBiographyEN}</p>
+            <p className={s.biography}>{artist.strBiographyEN}</p>
           </div>
-          {id.genres.map((genre) => {
+          {artist.genres.map((genre) => {
             return <li className={s.genre}>{genre}</li>;
           })}
           <div></div>
