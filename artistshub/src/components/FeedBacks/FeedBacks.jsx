8854584892434a4dc701d6e836023bf9da3b3016
alivieE <153372860+alivieE@users.noreply.github.com>
2025-10-11 12:34:23 +0300

    swiper

--- artistshub/src/components/FeedBacks/FeedBacks.jsx
+++ artistshub/src/components/FeedBacks/FeedBacks.jsx
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from "react";
 import s from "./FeedBacks.module.css";
 import StarRating from "./Rating.jsx";
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 const FeedBacks = () => {
   const [feedbacks, setFeedbacks] = useState([]);
@@ -21,14 +28,26 @@ const FeedBacks = () => {
           {feedbacks.length > 0 &&
             feedbacks.map((feedback) => {
               return (
-                <li>
-                  <StarRating
-                    value={feedback.rating}
-                    isChange={false}
-                  ></StarRating>
-                  <p className={s.description}>{feedback.descr}</p>
-                  <p className={s.name}>{feedback.name}</p>
-                </li>
+                <Swiper
+                  // install Swiper modules
+                  modules={[Navigation, Pagination, Scrollbar, A11y]}
+                  spaceBetween={50}
+                  slidesPerView={3}
+                  navigation
+                  pagination={{ clickable: true }}
+                  scrollbar={{ draggable: true }}
+                  onSwiper={(swiper) => console.log(swiper)}
+                  onSlideChange={() => console.log('slide change')}
+                >
+                  <li>
+                    <StarRating
+                      value={feedback.rating}
+                      isChange={false}
+                    ></StarRating>
+                    <p className={s.description}>{feedback.descr}</p>
+                    <p className={s.name}>{feedback.name}</p>
+                  </li>
+                </Swiper>     
               );
             })}
         </ul>
