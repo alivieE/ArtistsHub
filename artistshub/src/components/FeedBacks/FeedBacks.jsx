d1c0efcb04a4fde5f6be9d58769d0263f8fec4b8
MaksimRomanovvv <109852966+MaxRomanov5@users.noreply.github.com>
2025-10-11 11:52:56 +0200

    fix swioer js

--- artistshub/src/components/FeedBacks/FeedBacks.jsx
+++ artistshub/src/components/FeedBacks/FeedBacks.jsx
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from "react";
 import s from "./FeedBacks.module.css";
 import StarRating from "./Rating.jsx";
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 const FeedBacks = () => {
   const [feedbacks, setFeedbacks] = useState([]);
@@ -24,33 +28,38 @@ const FeedBacks = () => {
   return (
     <div className={s.section}>
       <div className="container">
-        <ul>
+        <Swiper
+          // install Swiper modules
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          spaceBetween={50}
+          slidesPerView={1}
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log("slide change")}
+        >
           {feedbacks.length > 0 &&
             feedbacks.map((feedback) => {
               return (
-                <Swiper
-                  // install Swiper modules
-                  modules={[Navigation, Pagination, Scrollbar, A11y]}
-                  spaceBetween={50}
-                  slidesPerView={3}
-                  navigation
-                  pagination={{ clickable: true }}
-                  scrollbar={{ draggable: true }}
-                  onSwiper={(swiper) => console.log(swiper)}
-                  onSlideChange={() => console.log('slide change')}
-                >
-                  <li>
+                <SwiperSlide>
+                  <div
+                    style={{
+                      paddingBottom: "40px",
+                      boxSizing: "border-box",
+                      height: "100%",
+                    }}
+                  >
                     <StarRating
                       value={feedback.rating}
                       isChange={false}
                     ></StarRating>
                     <p className={s.description}>{feedback.descr}</p>
                     <p className={s.name}>{feedback.name}</p>
-                  </li>
-                </Swiper>     
+                  </div>
+                </SwiperSlide>
               );
             })}
-        </ul>
+        </Swiper>
       </div>
     </div>
   );
